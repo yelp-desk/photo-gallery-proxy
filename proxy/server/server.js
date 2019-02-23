d1381efe0d4680097b286cc6e2297fb7cc82c196
Alex Harb <alex.harb11@gmail.com>
2019-02-23 09:38:52 -0800

    Allows proxy to run on aws

--- proxy/server/server.js
+++ proxy/server/server.js
@@ -2,10 +2,20 @@ const express = require('express');
 const morgan = require('morgan');
 const app = express();
 const port = process.env.PORT || 3000;
+const path = require('path');
+
 
 app.use(morgan('dev'));
-app.use(express.static((__dirname + '/../public/')));
+app.use(express.static(path.join(__dirname, '/../public')));
+
+app.get( '*', (req, res) => {
+  res.header({
+    'Cache-Control': 'no-cache'
+  })
+  res.sendFile(path.join(__dirname, '/../public/index.html'))
+});
 
 app.listen(port, () => {
   console.log(`server running at: http://localhost:${port}`);
 });
+
